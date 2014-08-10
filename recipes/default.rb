1e97a4020a23cc3aef1e4eb35b1c6ab90f0d526a
quantumz <gregory.weaver@gmail.com>
2014-08-10 16:34:42 -0700

    Making changes for from source install.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,10 @@
 #blacklist nouvea driver in /etc/modprobe.d/
+#blacklist nouvea
+# `/usr/lib/modprobe.d/blacklist-nouveau.conf` << blacklist nouveau
 #http://us.download.nvidia.com/XFree86/Linux-x86_64/340.24/NVIDIA-Linux-x86_64-340.24.run
 #chmod +x download
 # ./NVIDIA-Linux-x86_64-340.24.run --accept-license --update
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install kmod-nvidia
+yum install kmod-nvidia nvidia-x11-drv-32bit.x86_64
 yum remove xorg-x11-glamor
